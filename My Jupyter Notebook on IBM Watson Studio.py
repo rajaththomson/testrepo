9f825d423777039e182cfde0ae33075eb0a07533
rajaththomson <40268159+rajaththomson@users.noreply.github.com>
2022-11-14 16:16:48 +0530

    Add files via upload
    test file

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Mathew Rajath Thomson**
+
+# Product Manager
+
+# _I am interested in data science to explore astrophysics_
+
+# ### adding one plus one to give two
+
+# In[1]:
+
+
+print(1+1)
+
+
+# ***
+
+# <https://www.google.com>
+
+# > this is a blockquote
+
+# ~~that was a mistake~~ now it is correct
+
+# In[ ]:
+
+
+
+
